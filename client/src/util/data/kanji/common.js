3015e6d071cf0ddbd39eebaea93d8d69a2c3ada2
James <118614403+JumesP@users.noreply.github.com>
2025-06-05 01:10:30 +0100

    basic kanji characters

---
+++ client/src/util/data/kanji/common.js
@@ -0,0 +1,39 @@
+// Common kanji (people, actions, daily life)
+export const commonKanji = [
+    // People
+    { kanji: '人', meaning: 'person' },
+    { kanji: '女', meaning: 'woman' },
+    { kanji: '男', meaning: 'man' },
+    { kanji: '子', meaning: 'child' },
+    { kanji: '父', meaning: 'father' },
+    { kanji: '母', meaning: 'mother' },
+    { kanji: '友', meaning: 'friend' },
+    { kanji: '私', meaning: 'private, I' },
+
+    // Actions
+    { kanji: '見', meaning: 'see' },
+    { kanji: '行', meaning: 'go' },
+    { kanji: '来', meaning: 'come' },
+    { kanji: '出', meaning: 'exit' },
+    { kanji: '入', meaning: 'enter' },
+    { kanji: '話', meaning: 'talk' },
+    { kanji: '聞', meaning: 'hear' },
+    { kanji: '食', meaning: 'eat' },
+    { kanji: '読', meaning: 'read' },
+    { kanji: '書', meaning: 'write' },
+    { kanji: '思', meaning: 'think' },
+    { kanji: '知', meaning: 'know' },
+
+    // Places
+    { kanji: '家', meaning: 'house, family' },
+    { kanji: '社', meaning: 'company' },
+    { kanji: '校', meaning: 'school' },
+    { kanji: '駅', meaning: 'station' },
+    { kanji: '店', meaning: 'shop' },
+    { kanji: '市', meaning: 'city' },
+    { kanji: '町', meaning: 'town' },
+    { kanji: '村', meaning: 'village' },
+    { kanji: '都', meaning: 'metropolis' },
+    { kanji: '道', meaning: 'road, way' }
+];
+
